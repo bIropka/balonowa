3b3d639a7f6ec9b5c7445de574a552aafb207f01
Iegor_Stetsiura <biropka@gmail.com>
2017-10-01 01:48:58 +0300

    Tob block added

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -1,5 +1,10 @@
 $(window).ready(function() {
 
-
+    setTimeout(function() {
+        $('.wrapper').animate({opacity: 1}, 500);
+    }, 500);
 
 });
+
+var scene = document.getElementById('scene');
+var parallaxInstance = new Parallax(scene);
