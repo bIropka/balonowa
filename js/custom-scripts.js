e686ea35d4892716a77419e2ebe072b823c1a9aa
Iegor_Stetsiura <biropka@gmail.com>
2017-10-01 20:16:00 +0300

    Products added

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -4,7 +4,67 @@ $(window).ready(function() {
         $('.wrapper').animate({opacity: 1}, 500);
     }, 500);
 
+    /*******************************************
+    ****************** top parallax
+    *******************************************/
+
+    var scene = document.getElementById('scene');
+
+    var parallaxInstance = new Parallax(scene);
+
+    if($(window).width() < 1200) {
+        parallaxInstance.disable();
+    } else {
+        parallaxInstance.enable();
+    }
+
+    $(window).resize(function() {
+
+        if($(window).width() < 1200) {
+            parallaxInstance.disable();
+        } else {
+            parallaxInstance.enable();
+        }
+
+    });
+
+    // end of top parallax
+
+    /*******************************************
+    ******************* products
+    ********************************************/
+
+    changeCategory();
+
+    $('.product-filter-item').click(function() {
+        $(this).siblings().removeClass('active');
+        $(this).addClass('active');
+        changeCategory();
+    });
+
+    function changeCategory() {
+        var productCategories = $('.product-categories');
+        var categoryIndex = $('.product-filter').find('.active').attr('data-category');
+        $(productCategories).find('.active').removeClass('active');
+        $(productCategories).find('[data-category="' + categoryIndex + '"]').addClass('active');
+    }
+
+    $('.product-categories').on('click', '#preview-img-modal', function() {
+        $('#preview-img-modal').find('img').prependTo($('.product-image.active'));
+        $('#preview-img-modal').fadeOut();
+        setTimeout(function() {
+            $('#preview-img-modal').remove();
+        }, 500);
+        $('.product-image.active').removeClass('active');
+    });
+
+    $('.product-to-preview').click(function() {
+        $('<div id="preview-img-modal"></div>').appendTo($('.product-categories')).fadeIn();
+        $(this).parents('.product-image').find('img').appendTo($('#preview-img-modal'));
+        $(this).parents('.product-image').addClass('active');
+    });
+
+    // end of products
+
 });
 
-var scene = document.getElementById('scene');
-var parallaxInstance = new Parallax(scene);
