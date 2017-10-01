6c5d913f794924e6f16d20f741cd33aece6af86e
Iegor_Stetsiura <biropka@gmail.com>
2017-10-01 22:36:50 +0300

    Discount added

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -74,7 +74,41 @@ $(window).ready(function() {
         appendArrows: '.clients-control',
         prevArrow: '.clients-control-prev',
         nextArrow: '.clients-control-next',
-        slidesToShow: 3
+        slidesToShow: 3,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+
+    $('.slider-reviews').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 3,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
     });
 
     // end of sliders
